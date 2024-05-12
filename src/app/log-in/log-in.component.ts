8b30ba608abd100db8d6f3927b07dd39dbd3f506
mostri28 <mateoetchepare@gmail.com>
2024-05-12 20:32:18 -0300

    LogIn UX in progress

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'log-in',
   templateUrl: `log-in.component.html`,
 })
-export class LogInComponent { }
+export class LogInComponent implements OnInit { 
+  public logIn: boolean = true;
+
+  toggleLogIn() {
+    this.logIn = !this.logIn;
+    console.log(this.logIn)
+  }
+
+  ngOnInit(): void {
+      console.log('hello!');
+  }
+}
--- src/app/log-in/log-in.module.ts
+++ src/app/log-in/log-in.module.ts
@@ -1,11 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LogInComponent } from './log-in.component';
+import { RouterModule, Routes } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
+const routes: Routes = [
+  { path: '', component: LogInComponent } 
+]
 
 @NgModule({
   declarations: [LogInComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterModule.forChild(routes),
   ],
   exports: [
     LogInComponent,
